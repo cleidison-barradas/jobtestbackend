0ba0b413db781ece73fd4edb3d245a31b836eb69
cleidison <cleidisonalmei@hotmail.com>
2020-08-07 16:40:11 -0300

    put-user

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -2,9 +2,32 @@ import User from '../models/User';
 
 class UserController {
   async store(req, res) {
+    try{
+
     const user = await User.create(req.body);
 
     return res.json(user);
+    }catch(error){
+
+      return res.status(400).json({ error: error.message });
+    }
+  }
+  async put(req, res) {
+    try {
+      const { id } = req.params;
+      const userExists = await User.findById(id);
+
+      if ( !userExists) { 
+        return res.status(400).json({ error: 'user does not exists'});
+      }
+      await userExists.updateOne(req.body);
+
+      const user = await User.findById(id);
+
+      return res.json({ user });
+    } catch (error) {
+      return res.status(400).json({ error: error.message});
+    }
   }
 }
 export default new UserController();
