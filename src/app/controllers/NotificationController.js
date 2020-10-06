fbf52169a89bd0072d2d45ac7ea78f461b13150b
cleidison1994 <49909482+cleidison1994@users.noreply.github.com>
2020-10-06 10:19:41 -0300

    Update NotificationController.js

--- src/app/controllers/NotificationController.js
+++ src/app/controllers/NotificationController.js
@@ -4,6 +4,9 @@ class NotificationController {
   async index(req, res) {
     try {
       const { referenceId, authorizationId } = req.body;
+      res.headers = {
+        'x-seller-token': '4ee3b098-3cd2-4b58-9ba5-d5583d04e933',
+      };
 
       const response = {
         referenceId,
